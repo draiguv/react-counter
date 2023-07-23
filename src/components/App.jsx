c2066611969ff75f84982e2166ae3930b910f766
draiguv <dcrauv13@gmail.com>
2023-07-23 21:07:09 +0800

    Add functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
   return (
     <div className='container'>
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
